dc376e9a52847570b595811c54efd34ebbe5a30f
kurtmilam <kurt.milam@xioup.com>
2012-02-29 12:25:19 +0100

    Return response if parseBeforeLocalSave undefined
    Had been returning null, which obiously doesn't work if you're not using
    parseBeforeLocalSave.

--- backbone.dualstorage.js
+++ backbone.dualstorage.js
@@ -122,7 +122,7 @@
   };
 
   parseRemoteResponse = function(object, response) {
-    if (!(object && object.parseBeforeLocalSave)) return null;
+    if (!(object && object.parseBeforeLocalSave)) return response;
     if (_.isFunction(object.parseBeforeLocalSave)) {
       return object.parseBeforeLocalSave(response);
     }
